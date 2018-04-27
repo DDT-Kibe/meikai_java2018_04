f1684e27b517cd859d4dbdc1a649553fe833c78c
KK\KK <ddt.kibe@gmail.com>
2018-04-27 20:09:08 +0900

    明解Java　演習1-1 初回コミット

---
+++ e_01_01/src/e_01_01/e_01_01.java
@@ -0,0 +1,10 @@
+package e_01_01;
+
+public class e_01_01 {
+
+	public static void main(String[] args) {
+		System.out.println("初めてのJavaプログラム。");
+		System.out.println("画面に出力しています。")
+	}
+}
+//「;」がないと命令文の終わりをコンピュータが判断できないため、エラーとなると思われる。
