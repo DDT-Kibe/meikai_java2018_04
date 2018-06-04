0e776f8c250ec4cd35333d1e330f70034d95c954
KK\KK <ddt.kibe@gmail.com>
2018-06-04 20:58:19 +0900

    明解java　演習2-2　3回目コミット

--- meikai_java2018_05/src/e_02/e_02_02.java
+++ meikai_java2018_05/src/e_02/e_02_02.java
@@ -9,18 +9,21 @@ public class e_02_02 {
 	public static void main(String[] args) {
 		
 		//演習2-2を実施するためint型の変数を三つ用意する。
-		int hensu_1;           // hensu_1はint型の変数
-		int hensu_2;           // hensu_2はint型の変数
-		int hensu_3;           // hensu_3はint型の変数
+		int firstNumber;           // firstNumberはint型の変数
+		int secondNumber;          // secondNumberはint型の変数
+		int thirdNumber;           // thirdNumberはint型の変数
 
 		//演習2-2を実施するためint型の変数に整数を代入する。
-		hensu_1 = 10;           // hensu_1に10を代入する
-		hensu_2 = 20;           // hensu_2に20を代入する
-		hensu_3 = 30;           // hensu_3に30を代入する
+		firstNumber = 10;            // firstNumberに10を代入する
+		secondNumber = 20;           // secondNumberに20を代入する
+		thirdNumber = 30;            // thirdNumberに30を代入する
 
 		//演習2-2を実施するため合計を求めるプログラムを作成する。
-		System.out.println("hensu_1～hensu_3の合計は" +( hensu_1 + hensu_2 + hensu_3 ) + "です。");
+		System.out.println("firstNumber～thirdNumberの合計は"
+							+ (firstNumber + secondNumber + thirdNumber) + "です。");
+		
 		//演習2-2を実施するため平均を求めるプログラムを作成する。
-		System.out.println("hensu_1～hensu_3の平均は"  + ( hensu_1 + hensu_2 + hensu_3 ) / 3 + "です。");		
+		System.out.println("firstNumber～thirdNumberの平均は"
+							+ (firstNumber + secondNumber + thirdNumber) / 3 + "です。");		
 		}
 }
