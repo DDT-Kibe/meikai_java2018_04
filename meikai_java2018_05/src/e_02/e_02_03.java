ef2f94caf81cc2941daa85fe1cc540a2142b25a3
KK\KK <ddt.kibe@gmail.com>
2018-06-06 20:34:06 +0900

    明解java　演習2-3　初回コミット

---
+++ meikai_java2018_05/src/e_02/e_02_03.java
@@ -0,0 +1,24 @@
+package e_02;
+
+import java.util.Scanner;
+
+
+/*
+ * 演習2-3：キーボードから読み込んだ整数値をそのまま反復して表示するプログラムを作成せよ。
+ * 作成者：岐部 佳織
+ * 作成日：2018年6月6日
+ */
+public class e_02_03 {
+	
+	public static void main(String[] args) {
+		
+		//演習2-3を実施するためキーボードから読み込ませる。
+		Scanner stdIn = new Scanner(System.in);		
+		//演習2-3を実施するためint型整数値を読み込ませる。
+		int repetitionInput = stdIn.nextInt();
+		
+		//演習2-3を実施するため変数に代入される値ををのまま表示させる。
+		System.out.print(repetitionInput + "と入力しましたね。");		
+	}
+
+}
