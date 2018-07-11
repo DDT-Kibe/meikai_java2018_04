005a6b89c06f5b30b3fc32ddcbacf2c9170636b2
KK\KK <ddt.kibe@gmail.com>
2018-07-11 21:29:13 +0900

    [WIP]明解java　演習3-3
    当日行った内容：List3-5を途中まで転記。50％完了
    次回ToDo：List3-5の転記とコメント入力。

---
+++ meikai_java2018_05/src/e_03/e_03_03.java
@@ -0,0 +1,31 @@
+package e_03;
+
+import java.util.Scanner;
+/*
+ * 演習3-3：List3-5の最後の else を、else if(n == 0) に変更したらどうなるかを検討せよ。
+ * 作成者：岐部 佳織
+ * 作成日：2018年7月■日
+ */
+public class e_03_03 {
+
+	public static void main(String[] args) {
+
+	//キーボードからの入力を受け取る用意をする。
+	Scanner inKeyboard = new Scanner(System.in);
+
+	//タイトルを表示させ、整数値の入力を促す。
+	System.out.print("整数値：");
+	//入力された整数値を取得する。
+	int integerValue = inKeyboard.nextInt();
+
+
+	/*List3-5を転記途中です。
+	if(n > 0) {
+		System.out.println("その値は正です");
+	}
+
+	*/
+
+
+	}
+}
