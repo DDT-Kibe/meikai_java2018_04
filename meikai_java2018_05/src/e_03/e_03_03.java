386948037f818520a3aa944c4d1391836f8e3706
KK\KK <ddt.kibe@gmail.com>
2018-07-23 20:26:21 +0900

    明解java　演習3-3　初回コミット

--- meikai_java2018_05/src/e_03/e_03_03.java
+++ meikai_java2018_05/src/e_03/e_03_03.java
@@ -4,28 +4,37 @@ import java.util.Scanner;
 /*
  * 演習3-3：List3-5の最後の else を、else if(n == 0) に変更したらどうなるかを検討せよ。
  * 作成者：岐部 佳織
- * 作成日：2018年7月■日
+ * 作成日：2018年7月23日
  */
 public class e_03_03 {
 
 	public static void main(String[] args) {
 
-	//キーボードからの入力を受け取る用意をする。
-	Scanner inKeyboard = new Scanner(System.in);
+		//キーボードからの入力を受け取る用意をする。
+		Scanner inKeyboard = new Scanner(System.in);
 
-	//タイトルを表示させ、整数値の入力を促す。
-	System.out.print("整数値：");
-	//入力された整数値を取得する。
-	int integerValue = inKeyboard.nextInt();
+		//タイトルを表示させ、整数値の入力を促す。
+		System.out.print("整数値：");
+		//入力された整数値を取得する。
+		int integerValue = inKeyboard.nextInt();
 
-
-	/*List3-5を転記途中です。
-	if(n > 0) {
-		System.out.println("その値は正です");
-	}
-
-	*/
-
-
+		//入力された値が0よりも大きい場合、
+		if(integerValue > 0) {
+			//その値が正である旨を表示する。
+			System.out.print("その値は正です。");
+    	}
+		//入力された値が0よりも小さい場合、
+		else if(integerValue < 0){
+			//その値が負である旨を表示する。
+			System.out.print("その値は負です。");
+    	}
+		//入力された値が0である場合、
+		else if(integerValue == 0) {
+			//その値が0である旨を表示する。
+			System.out.print("その値は0です。");
+		}
+		//検討結果
+		//「else」のときと処理結果は同じだが、工程が異なる。
+		//「else if(integerValue == 0)」だと条件が3つになり、「else」だと条件は2つになる。
 	}
 }
