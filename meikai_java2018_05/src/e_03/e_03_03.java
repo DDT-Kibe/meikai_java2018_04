b53536ac5531566cb35409e42a7141eb2b920e4b
KK\KK <ddt.kibe@gmail.com>
2018-07-23 21:30:54 +0900

    明解java　演習3-3　2回目コミット ブロックの修正、コメントの修正をしました。

--- meikai_java2018_05/src/e_03/e_03_03.java
+++ meikai_java2018_05/src/e_03/e_03_03.java
@@ -8,33 +8,34 @@ import java.util.Scanner;
  */
 public class e_03_03 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		//キーボードからの入力を受け取る用意をする。
-		Scanner inKeyboard = new Scanner(System.in);
+    //キーボードからの入力を受け取る用意をする。
+    Scanner inKeyboard = new Scanner(System.in);
 
-		//タイトルを表示させ、整数値の入力を促す。
-		System.out.print("整数値：");
-		//入力された整数値を取得する。
-		int integerValue = inKeyboard.nextInt();
+    //タイトルを表示させ、整数値の入力を促す。
+    System.out.print("整数値：");
+    //入力された整数値を取得する。
+    int integerValue = inKeyboard.nextInt();
 
-		//入力された値が0よりも大きい場合、
-		if(integerValue > 0) {
-			//その値が正である旨を表示する。
-			System.out.print("その値は正です。");
-    	}
-		//入力された値が0よりも小さい場合、
-		else if(integerValue < 0){
-			//その値が負である旨を表示する。
-			System.out.print("その値は負です。");
-    	}
-		//入力された値が0である場合、
-		else if(integerValue == 0) {
-			//その値が0である旨を表示する。
-			System.out.print("その値は0です。");
-		}
-		//検討結果
-		//「else」のときと処理結果は同じだが、工程が異なる。
-		//「else if(integerValue == 0)」だと条件が3つになり、「else」だと条件は2つになる。
-	}
+    //入力された値が0よりも大きい場合、
+    if(integerValue > 0){
+      //その値が正である旨を表示する。
+      System.out.print("その値は正です。");
+
+    //入力された値が0よりも小さい場合、
+    }else if(integerValue < 0){
+      //その値が負である旨を表示する。
+      System.out.print("その値は負です。");
+
+    //入力された値が0である場合、
+    }else if(integerValue == 0){
+      //その値が0である旨を表示する。
+      System.out.print("その値は0です。");
+    }
+    //検討結果
+    //「else」のときと処理結果は同じだが、工程が異なる。
+    //「else if(integerValue == 0)」だと条件が3つ。
+    //「else」でも条件は3つだが、「 > 0」の場合、「 < 0」の場合、そして「それ以外」となる。
+  }
 }
