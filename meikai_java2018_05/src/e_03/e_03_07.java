fd6381ef658fec5b6eb8d243d31c8462ff1bcf2a
KK\KK <ddt.kibe@gmail.com>
2018-08-01 21:02:38 +0900

    明解java　演習3-7　3回目コミット 初期値の設定について修正。 　・位置をmain文の下に移動。 　・変数名をスネークケースで修正。

--- meikai_java2018_05/src/e_03/e_03_07.java
+++ meikai_java2018_05/src/e_03/e_03_07.java
@@ -13,6 +13,15 @@ public class e_03_07 {
 
     public static void main(String[] args) {
 
+        //割る数を初期値で設定する。
+        final int DIVISION_NUMBER = 3;
+        //割り切れるか否かの判断で使用する値を初期値で設定する。
+        final int NUMBER_DIVISIBLE = 0;
+        //「余りは1」の判断で使用する値を初期値で設定する。
+        final int SURPLUS_JUDGMENT_ONE = 1;
+        //「余りは2」の判断で使用する値を初期値で設定する。
+        final int SURPLUS_JUDGMENT_TWO = 2;
+        
         //キーボードからの入力を受け取る用意をする。
         Scanner inKeyboard = new Scanner(System.in);
 
@@ -21,29 +30,20 @@ public class e_03_07 {
         //入力された値を取得する。
         int integerValue = inKeyboard.nextInt();
 
-        //割る数を初期値で設定する。
-        final int divisionNumber = 3;
-        //割り切れるか否かの判断で使用する値を初期値で設定する。
-        final int numberDivisible = 0;
-        //「余りは1」の判断で使用する値を初期値で設定する。
-        final int surplusJudgmentOne = 1;
-        //「余りは2」の判断で使用する値を初期値で設定する。
-        final int surplusJudgmentTwo = 2;
-
         //正でない値を読み込んだ場合、
         if (integerValue <= 0) {
             //『正でない値が入力されました』を表示する。
             System.out.println("正でない値が入力されました");
         //入力された値が3で割り切れる場合、
-        }else if (integerValue % divisionNumber == numberDivisible) {
+        }else if (integerValue % DIVISION_NUMBER == NUMBER_DIVISIBLE) {
             //『その値は3で割り切れます』を表示する。
             System.out.println("その値は3で割り切れます");
         //入力された値を3で割り余りが1の場合、
-        }else if (integerValue % divisionNumber == surplusJudgmentOne) {
+        }else if (integerValue % DIVISION_NUMBER == SURPLUS_JUDGMENT_ONE) {
             //『その値を3で割った余りは1です』を表示する。
             System.out.println("その値を3で割った余りは1です");
         //入力された値を3で割り余りが2の場合、
-        }else if (integerValue % divisionNumber == surplusJudgmentTwo) {
+        }else if (integerValue % DIVISION_NUMBER == SURPLUS_JUDGMENT_TWO) {
             //『その値を3で割った余りは2です』を表示する。
             System.out.println("その値を3で割った余りは2です");
         }
