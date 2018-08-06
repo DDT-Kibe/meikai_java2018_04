9d49c229fac471eaa39364d9f9989af7b3f5ede2
KK\KK <ddt.kibe@gmail.com>
2018-08-06 21:12:58 +0900

    明解java　演習3-8　初回コミット

--- meikai_java2018_05/src/e_03/e_03_08.java
+++ meikai_java2018_05/src/e_03/e_03_08.java
@@ -6,31 +6,28 @@ import java.util.Scanner;
  *          判定は以下のように行うこと。
  *            0～59 → 不可 ／ 60～69 → 可 ／ 70～79 → 良 ／ 80～100 → 優
  * 作成者：岐部 佳織
- * 作成日：2018年8月■日
+ * 作成日：2018年8月6日
  */
 public class e_03_08 {
 
     public static void main(String[] args) {
 
-        //「不可」の範囲指定で使用する、はじまりの値を初期値で設定する。
-        final int WORST_MARK = 0;
-        //「不可」の範囲指定で使用する、おわりの値を初期値で設定する。
-        final int USELESS_MARK = 59;
-
-        /*変数名考え中
-        //「可」の範囲指定で使用する、はじまりの値を初期値で設定する。
-        final int ccc_MARK = 60;
-        //「可」の範囲指定で使用する、おわりの値を初期値で設定する。
-        final int ddd_MARK = 69;
-        //「良」の範囲指定で使用する、はじまりの値を初期値で設定する。
-        final int eee_MARK = 70;
-        //「良」の範囲指定で使用する、おわりの値を初期値で設定する。
-        final int fff_MARK = 79;
-        //「優」の範囲指定で使用する、はじまりの値を初期値で設定する。
-        final int ggg_MARK = 80;
-        //「優」の範囲指定で使用する、おわりの値を初期値で設定する。
-        final int hhh_MARK = 100;
-        */
+        //「不可」の範囲指定で使用する、はじまりの点数を初期値で設定する。
+        final int START_WORST_MARK = 0;
+        //「不可」の範囲指定で使用する、おわりの点数を初期値で設定する。
+        final int END_WORST_MARK = 59;
+        //「可」の範囲指定で使用する、はじまりの点数を初期値で設定する。
+        final int START_USELESS_MARK = 60;
+        //「可」の範囲指定で使用する、おわりの点数を初期値で設定する。
+        final int END_USELESS_MARK = 69;
+        //「良」の範囲指定で使用する、はじまりの点数を初期値で設定する。
+        final int START_GOOD_MARK = 70;
+        //「良」の範囲指定で使用する、おわりの点数を初期値で設定する。
+        final int END_GOOD_MARK = 79;
+        //「優」の範囲指定で使用する、はじまりの点数を初期値で設定する。
+        final int START_GREAT_MARK = 80;
+        //「優」の範囲指定で使用する、おわりの点数を初期値で設定する。
+        final int END_GREAT_MARK = 100;
 
         //キーボードからの入力を受け取る用意をする。
         Scanner inKeyboard = new Scanner(System.in);
@@ -40,19 +37,22 @@ public class e_03_08 {
         //入力された値を取得する。
         int integerValue = inKeyboard.nextInt();
 
-        //、
-        if (integerValue >= WORST_MARK && integerValue <= USELESS_MARK) {
-            //
+        //点数が不可の範囲内である場合、
+        if (integerValue >= START_WORST_MARK && integerValue <= END_WORST_MARK) {
+            //不可である旨を表示する。
             System.out.println("不可");
-
-        /*以下、作成中
-        //、
-        }else if (integerValue >= ccc_MARK && integerValue <= ddd_MARK) {
-            //
+        //点数が可の範囲内である場合、
+        }else if (integerValue >= START_USELESS_MARK && integerValue <= END_USELESS_MARK) {
+            //可である旨を表示する。
             System.out.println("可");
-            */
-
-
+        //点数が良の範囲内である場合、
+        }else if (integerValue >= START_GOOD_MARK && integerValue <= END_GOOD_MARK) {
+            //良である旨を表示する。
+            System.out.println("良");
+        //点数が優の範囲内である場合、
+        }else if (integerValue >= START_GREAT_MARK && integerValue <= END_GREAT_MARK) {
+            //優である旨を表示する。
+            System.out.println("優");
         }
     }
 }
