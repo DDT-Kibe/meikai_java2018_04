77a98cd3529f4687cb319168da891a88c2607dd4
KK\KK <ddt.kibe@gmail.com>
2018-08-22 19:56:20 +0900

    明解java　演習3-10　初回コミット

--- meikai_java2018_05/src/e_03/e_03_10.java
+++ meikai_java2018_05/src/e_03/e_03_10.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /*
  * 演習3-10：二つの整数値を読み込んで、それらの値の差を表示するプログラムを作成せよ。
  * 作成者：岐部 佳織
- * 作成日：2018年8月■日
+ * 作成日：2018年8月22日
  */
 public class e_03_10 {
 
@@ -17,20 +17,19 @@ public class e_03_10 {
         System.out.print("値Ａ：");
         //入力された整数値を取得する。
         int formerInteger = inKeyboard.nextInt();
-
         //タイトルを表示させ、整数値Ｂの入力を促す。
         System.out.print("値Ｂ：");
         //入力された整数値を取得する。
         int latterInteger = inKeyboard.nextInt();
 
         //整数値Ａの方が大きい場合の差を計算する。
-        int ddd = formerInteger - latterInteger;
+        int formerSubtraction = formerInteger - latterInteger;
         //整数値Ｂの方が大きい場合の差を計算する。
-        int eee = latterInteger - formerInteger;
+        int latterSubtraction = latterInteger - formerInteger;
         //整数値Ａと整数値Ｂを比較し、差を計算する。、
-        int fff = (formerInteger > latterInteger ? (ddd) : (eee));
+        int calculationResult = (formerInteger > latterInteger ? (formerSubtraction) : (latterSubtraction));
 
         //整数値Ａと整数値Ｂの差を示す。
-        System.out.println("値Ａと値Ｂの差は " + fff + " です");
+        System.out.println("値Ａと値Ｂの差は " + calculationResult + " です");
     }
 }
