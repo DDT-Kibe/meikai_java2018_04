6bfbd028e7eda604b9a05b32cc75019f9aad7b28
KK\KK <ddt.kibe@gmail.com>
2018-09-05 21:13:58 +0900

    明解java　演習3-13　初回コミット

--- meikai_java2018_05/src/e_03/e_03_13.java
+++ meikai_java2018_05/src/e_03/e_03_13.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * 演習3-13：キーボードから読み込んだ三つの整数値の中央値を求めて表示するプログラムを作成せよ。
  *           ※たとえば、2,3,1の中央値は2で、1,2,1,の中央値は1で、3,3,3の中央値は3である。
  * 作成者：岐部 佳織
- * 作成日：2018年9月■日
+ * 作成日：2018年9月5日
  */
 public class e_03_13 {
 
@@ -30,16 +30,33 @@ public class e_03_13 {
 
         //中央値を示す変数を設け、一つ目の入力値を代入する。
         int mediumValue = firstInteger;
-        //
-        if (mediumValue == thirdInteger || mediumValue == secondInteger)
-            //
-        /*
-         *
-         * 考え中
-         *
-         *
-        */
-        //どの入力値が中央値かを表示する。
+        //一つ目の入力値が最小値である場合、
+        if (mediumValue < secondInteger && mediumValue < thirdInteger) {
+            //二つ目の入力値が三つ目の入力値以下であることを判定し、
+            if (secondInteger <= thirdInteger) {
+                //trueの場合、中央値を示す変数に二つ目の入力値を代入する。
+                mediumValue = secondInteger;
+                //falseの場合、
+            } else {
+                //中央値を示す変数に三つ目の入力値を代入する。
+                mediumValue = thirdInteger;
+            }
+            //一つ目の入力値が最大値である場合、
+        } else if (mediumValue > secondInteger && mediumValue > thirdInteger) {
+            //二つ目の入力値が三つ目の入力値以上であることを判定し、
+            if (secondInteger >= thirdInteger) {
+                //trueの場合、中央値を示す変数に二つ目の入力値を代入する。
+                mediumValue = secondInteger;
+                //falseの場合、
+            } else {
+                //中央値を示す変数に三つ目の入力値を代入する。
+                mediumValue = thirdInteger;
+            }
+            //二つ目と三つ目の入力値のどちらか一方でも一つ目の入力値と同じ場合、変数には何もしない。
+        } else if (mediumValue == secondInteger || mediumValue == thirdInteger) {
+
+        }
+        //中央値を表示する。
         System.out.println("中央値は" + mediumValue + "です");
     }
 }
