37350233f989aa9b8179278a2386fcfbfe179b7a
KK\KK <ddt.kibe@gmail.com>
2018-09-10 21:21:48 +0900

    明解java　演習3-14　初回コミット ※WIPで写していたプログラムは、List3-13ではなくList3-14でした。

--- meikai_java2018_05/src/e_03/e_03_14.java
+++ meikai_java2018_05/src/e_03/e_03_14.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  *           両方を表示するプログラムを作成せよ。
  *           ただし、二つの整数値が等しい場合は、『二つの値は同じです』と表示すること。
  * 作成者：岐部 佳織
- * 作成日：2018年9月■日
+ * 作成日：2018年9月10日
  */
 public class e_03_14 {
 
@@ -16,30 +16,37 @@ public class e_03_14 {
         //キーボードからの入力を受け取る用意をする。
         Scanner inKeyboard = new Scanner(System.in);
 
-        //タイトルを表示させ、変数ａの入力を促す。
-        System.out.print("変数ａ：");
+        //タイトルを表示させ、整数ａの入力を促す。
+        System.out.print("整数ａ：");
         //入力された整数値を取得する。
         int variableIntegerA = inKeyboard.nextInt();
-        //タイトルを表示させ、変数ｂの入力を促す。
-        System.out.print("変数ｂ：");
+        //タイトルを表示させ、整数ｂの入力を促す。
+        System.out.print("整数ｂ：");
         //入力された整数値を取得する。
         int variableIntegerB = inKeyboard.nextInt();
 
-        //※※以下、List3-13のプログラムを写しただけ。※※
-        //
-        if (variableIntegerA > variableIntegerB) {
-            //
-            int tt = variableIntegerA;
-            //
-            variableIntegerA = variableIntegerB;
-            //
-            variableIntegerB = tt;
+        //小さい方の値を示す変数を設ける。
+        int minimumValue;
+        //大きい方の値を示す変数を設ける。
+        int maximumValue;
+        //整数ａが整数ｂよりも小さいことを判定し、
+        if (variableIntegerA < variableIntegerB) {
+            //trueの場合、小さい方の値を示す変数に整数ａを代入し、
+            minimumValue = variableIntegerA;
+            //大きい方の値を示す変数に整数ｂを代入する。
+            maximumValue = variableIntegerB;
+        //falseの場合、
+        }else {
+            //小さい方の値を示す変数に整数ｂを代入し、
+            minimumValue = variableIntegerB;
+            //大きい方の値を示す変数に整数ａを代入する。
+            maximumValue = variableIntegerA;
         }
-        //
-        System.out.println("a≦bになるようにソートしました");
-        //
-        System.out.println("変数aは" + variableIntegerA + "です");
-        //
-        System.out.println("変数bは" + variableIntegerB + "です");
+        //表示をする際に整数ａと整数ｂが同じ値であるかを判定し、trueの場合、値が同じである旨を表示する。
+        System.out.println((variableIntegerA == variableIntegerB) ? "二つの値は同じです"
+                //falseの場合、小さい方の値を先に表示し、
+                : "小さい方の値は" + minimumValue + "です"
+                //次に大きい方の値を表示する。
+                + "\n大きい方の値は" + maximumValue + "です");
     }
 }
