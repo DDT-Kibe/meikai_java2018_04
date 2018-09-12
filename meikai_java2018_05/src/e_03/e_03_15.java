d4c26977572bda07beedff1ea043e9ee210d1440
KK\KK <ddt.kibe@gmail.com>
2018-09-12 20:44:28 +0900

    明解java　演習3-15　初回コミット

--- meikai_java2018_05/src/e_03/e_03_15.java
+++ meikai_java2018_05/src/e_03/e_03_15.java
@@ -1,10 +1,11 @@
 package e_03;
 
 import java.util.Scanner;
+
 /*
  * 演習3-15：二つの整数値を読み込んで降順（大きい順）にソートするプロフラムを作成せよ。
  * 作成者：岐部 佳織
- * 作成日：2018年9月■日
+ * 作成日：2018年9月12日
  */
 public class e_03_15 {
 
@@ -16,28 +17,26 @@ public class e_03_15 {
         //タイトルを表示させ、整数ａの入力を促す。
         System.out.print("整数ａ：");
         //入力された整数値を取得する。
-        int IntegerValueA = inKeyboard.nextInt();
+        int variableIntegerA = inKeyboard.nextInt();
         //タイトルを表示させ、整数ｂの入力を促す。
         System.out.print("整数ｂ：");
         //入力された整数値を取得する。
-        int IntegerValueB = inKeyboard.nextInt();
+        int variableIntegerB = inKeyboard.nextInt();
 
-        //コメントを考え中。
-        //
-        if (IntegerValueA < IntegerValueB) {
-            //
-            int tt = IntegerValueA;
-            //
-            IntegerValueA = IntegerValueB;
-            //
-            IntegerValueB = tt;
+        //整数ａが整数ｂよりも小さい値であることを判定する。
+        if (variableIntegerA < variableIntegerB) {
+            //整数ａが整数ｂよりも小さい値の場合、作業用の変数を設け整数ａの値を代入する。
+            int temporaryVariable = variableIntegerA;
+            //整数ａの変数に整数ｂの値を代入する。
+            variableIntegerA = variableIntegerB;
+            //整数ｂの変数に作業用として保存していた値を代入する。
+            variableIntegerB = temporaryVariable;
         }
-        //
-        System.out.println("\n整数を降順にソートしました");
-        //
-        System.out.println(IntegerValueA);
-        //
-        System.out.println(IntegerValueB);
-
+        //降順でソートした旨を表示する。
+        System.out.println("\nａ≧ｂとなるようソートしました");
+        //大きい方の値を表示する。
+        System.out.println("変数ａ：" + variableIntegerA);
+        //小さい方の値を表示する。
+        System.out.println("変数ｂ：" + variableIntegerB);
     }
 }
