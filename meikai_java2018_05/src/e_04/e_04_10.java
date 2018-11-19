37476e7c5e03b8982f7dc6b993fc5496cde75f4a
KK\KK <ddt.kibe@gmail.com>
2018-11-19 21:15:50 +0900

    明解java　演習4-10　初回コミット

---
+++ meikai_java2018_05/src/e_04/e_04_10.java
@@ -0,0 +1,31 @@
+package e_04;
+
+import java.util.Scanner;
+
+/*
+ * 演習4-10：読み込んだ値が1未満であれば改行文字を出力しないように
+ *           List4-11（p.114）を書きかえたプログラムを作成せよ。
+ * 作成者：岐部 佳織
+ * 作成日：2018年11月19日
+ */
+public class e_04_10 {
+
+    public static void main(String[] args) {
+        //変数を設け符号の判定で使用する閾値を設定する。
+        final int THRESHOLD_VALUE = 0;
+        //キーボードからの入力を受け取る用意をする。
+        Scanner inKeyboard = new Scanner(System.in);
+        //タイトルを表示させ、入力を促す。
+        System.out.print("何個*を表示しますか：");
+        //入力された値を取得する。
+        int requestedValue = inKeyboard.nextInt();
+        //for文でまずrepeatValueに閾値を代入する。入力値の方が大きい間は、repeatValueの値をインクリメントし、
+        for (int repeatValue = THRESHOLD_VALUE; repeatValue < requestedValue; repeatValue++)
+            //「*」を表示する。
+            System.out.print('*');
+        //入力値が閾値よりも大きい場合、
+        if (requestedValue > THRESHOLD_VALUE)
+            //改行文字を表示する。
+            System.out.println('\n');
+    }
+}
