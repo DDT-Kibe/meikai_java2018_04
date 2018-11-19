d5ffedcdb1d0950a676aaaf6efede75579b9095e
KK\KK <ddt.kibe@gmail.com>
2018-11-19 20:55:52 +0900

    明解java　演習4-9　初回コミット

--- meikai_java2018_05/src/e_04/e_04_09.java
+++ meikai_java2018_05/src/e_04/e_04_09.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 /*
  * 演習4-9：正の整数値nを読み込んで、1からnまでの積を求めるプログラムを作成せよ。
  * 作成者：岐部 佳織
- * 作成日：2018年11月■日
+ * 作成日：2018年11月19日
  */
 public class e_04_09 {
 
     public static void main(String[] args) {
         //変数を設け符号の判定で使用する閾値を設定する。
         final int THRESHOLD_VALUE = 0;
-        //はじまりの数～～～～コメント考え中
-        final int fff = 1;
+        //計算がはじまる値を設定する。
+        final int BEGINNING_NUMBER = 1;
         //キーボードからの入力を受け取る用意をする。
         Scanner inKeyboard = new Scanner(System.in);
-
         //入力された値を取得する。
         int requestedValue;
         //do文を記述していく。
@@ -32,23 +31,23 @@ public class e_04_09 {
             }
             //入力値が閾値以下の場合は処理を繰り返す。
         } while (requestedValue <= THRESHOLD_VALUE);
-        //籍を示す変数を設け1を代入する。
-        int repeatValue = fff;
-        //処理数を数えるために使用する変数を設け1を代入する。
-        int sss = fff;
-        //～～～～コメント考え中
+        //積を示す変数を設け1を代入する。
+        int totalProduct = BEGINNING_NUMBER;
+        //処理数を数えるための変数を設け1を代入する。
+        int repeatValue = BEGINNING_NUMBER;
+        //入力値が閾値よりも大きい場合、
         while (requestedValue > THRESHOLD_VALUE) {
-            //～～～～コメント考え中
-            repeatValue *= sss;
-            //～～～～コメント考え中
-            if (sss == requestedValue) {
-                //～～～～コメント考え中
+            //積を示す変数にrepeatValueの値を掛ける。
+            totalProduct *= repeatValue;
+            //repeatValueの値が入力値と等しい場合、
+            if (repeatValue == requestedValue) {
+                //処理を抜ける。
                 break;
             }
-            //sssに代入されている値をインクリメントし数を数えていく。
-            sss++;
+            //repeatValueの値をインクリメントし処理数を数えていく。
+            repeatValue++;
         }
         //1から入力値までの積を表示する。
-        System.out.print("1から" + requestedValue + "までの積は" + repeatValue + "です。");
+        System.out.print("1から" + requestedValue + "までの積は" + totalProduct + "です。");
     }
 }
