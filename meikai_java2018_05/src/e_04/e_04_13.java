27113e8f5a41deddbf80378b0cac2280b74700a6
KK\KK <ddt.kibe@gmail.com>
2018-12-10 21:29:17 +0900

    明解java　演習4-13　2回目コミット コメントと改行を修正しました。

--- meikai_java2018_05/src/e_04/e_04_13.java
+++ meikai_java2018_05/src/e_04/e_04_13.java
@@ -11,33 +11,35 @@ public class e_04_13 {
 
     public static void main(String[] args) {
 
-        //定数を設け加算処理をする際に最初に足される値を設定する。
+        //加算処理を開始する前に初期化する値。
         final int SPECIFIC_NUMBER = 0;
-        //定数を設け和を求める範囲の始まりの値（閾値）を設定する。
+        //和を求める範囲の始まりの値（閾値）。
         final int THRESHOLD_VALUE = 1;
+
         //キーボードからの入力を受け取る用意をする。
         Scanner inKeyboard = new Scanner(System.in);
         //nまでの和を求める旨を表示する。
         System.out.println("1からnまでの和を求めます。");
-        //入力値を代入する変数を設ける。
+
+        //入力値を受け取る用意をする。
         int requestedValue;
-        //do文を記述していく。
+        //入力値が適当であるかを判定する。
         do {
             //タイトルを表示し、入力を促す。
             System.out.print("nの値：");
             //入力された値を取得する。
             requestedValue = inKeyboard.nextInt();
-            //入力値が閾値よりも小さい場合は処理を繰り返す。
+            //入力値が閾値よりも小さいかを判定する。
         } while (requestedValue < THRESHOLD_VALUE);
-        //閾値から入力値までの和を表示する変数を設け、最初に足される値を代入する。
+
+        //加算処理を開始する前に足される値を初期化する。
         int totalValue = SPECIFIC_NUMBER;
-        //1から入力値に到達するまで繰り返し加算処理を行うために作業用の変数additionProcessingを設け、
-        //additionProcessingが入力値以下の間は下記の処理を行う。
+        //足される値が入力値以上になるまで判定を繰り返し以下の処理を行う。
         for (int additionProcessing = THRESHOLD_VALUE; additionProcessing <= requestedValue; additionProcessing++) {
             //加算処理を行う。
             totalValue += additionProcessing;
         }
-        //1から入力値までの和を表示する。
+        //加算処理した結果を表示する。
         System.out.println("1から" + requestedValue + "までの和は" + totalValue + "です。");
     }
 }
